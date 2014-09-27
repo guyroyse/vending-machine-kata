8de9aa190c77c9b1349438bad8a94c59d5353c14
Rob Kinyon <rob.kinyon@gmail.com>
2014-09-27 14:05:30 -0400

    issue_5: Reject invalid input

--- spec/vending_machine_spec.rb
+++ spec/vending_machine_spec.rb
@@ -110,5 +110,15 @@ describe VendingMachine do
         "'blah' rejected\nINSERT COIN\n",
       ])
     end
+
+    it "responds to 'quarter'" do
+      set_input("quarter\n", "q\n")
+
+      vend.start
+
+      expect_output([
+        "$0.25\n",
+      ])
+    end
   end
 end
